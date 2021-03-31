478d927c3c0fce7c1af667900f4a0662fb94cdbb
Christopher Banaszak <ludachris@Christophers-MacBook-Pro.local>
2021-03-30 21:23:01 -0500

    added very simple search bar and state functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+type Props = {};
+type State = {artistSearch: string, relatedArtists:any };
+
+export class App extends React.Component<Props, State>{
+
+  constructor(props:any){
+    super(props);
+
+    this.state = {
+      artistSearch: "",
+      relatedArtists: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit = () => {
+
+  };
+
+  handleChange = (event: { target: { value: string } }) => {
+    this.setState({artistSearch: event.target.value});
+  }
+
+  render(){
   return (
     <div className="App">
       <h1>ily</h1>
+
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Artist: 
+            <input type="text" value={this.state.artistSearch} onChange={this.handleChange}/>    
+        </label>
+        <input type="submit" value="tyl" />
+
+      </form>
     </div>
   );
+}
 }
 
 export default App;
